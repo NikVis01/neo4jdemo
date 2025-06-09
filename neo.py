7438dcd7130d68d9ea5ea4d09822d3dc61a69271
Nik Vid <niklavsvisockis@fastmail.se>
2025-06-09 13:34:22 +0200

    class for neo scripting

--- neo.py
+++ neo.py
@@ -1,19 +1,33 @@
 from neo4j import GraphDatabase
 
+"""
 URI = "bolt://localhost:7687"
 USER = "neo4j"
 PASSWORD = "twigaisawesome"
-
-driver = GraphDatabase.driver(URI, auth=(USER, PASSWORD))
+"""
 
 # print(type(driver))
+class QueryDB():
+    def __init__(self):
+        self.URI = "bolt://localhost:7687"
+        self.USER = "neo4j"
+        self.PASSWORD = "twigaisawesome"
 
-def get_anything(tx): # tx is the transaction object w method run() for cypher scripts in neo4j
-    result = tx.run("MATCH (n) RETURN n")
-    return [record.data() for record in result]
+        self.driver = GraphDatabase.driver(self.URI, auth=(self.USER, self.PASSWORD))
 
-with driver.session() as session:
-    nodes = session.execute_read(get_anything) # Don't need to pass tx, session.execute_read injects the parameter
-    print(nodes)
 
-driver.close()
+    def get_anything(self, tx): # tx is the transaction object w method run() for cypher scripts in neo4j
+        result = tx.run("MATCH (n) RETURN n")
+        return [record.data() for record in result]
+
+
+    def get_nodes(self):
+        with self.driver.session() as session:
+            nodes = session.execute_read(self.get_anything) # Don't need to pass tx, session.execute_read injects the parameter
+            print(nodes)
+
+
+        self.driver.close()  
+
+obj = QueryDB()
+obj.get_nodes()

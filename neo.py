95f6bb5a523f4d7bba6c739c1ee963d928967721
Nik Vid <niklavsvisockis@fastmail.se>
2025-06-09 15:01:41 +0200

    QUERYING LIVE

--- neo.py
+++ neo.py
@@ -1,4 +1,5 @@
 from neo4j import GraphDatabase
+from string import Template
 
 """
 URI = "bolt://localhost:7687"
@@ -8,26 +9,37 @@ PASSWORD = "twigaisawesome"
 
 # print(type(driver))
 class QueryDB():
-    def __init__(self):
+    def __init__(self): # Make theme passable from LLM output and password an .env var
         self.URI = "bolt://localhost:7687"
         self.USER = "neo4j"
         self.PASSWORD = "twigaisawesome"
 
         self.driver = GraphDatabase.driver(self.URI, auth=(self.USER, self.PASSWORD))
 
+        self.theme = "Fishing"
 
-    def get_anything(self, tx): # tx is the transaction object w method run() for cypher scripts in neo4j
-        result = tx.run(""""
-                        // Script for querying
+    def get_body_text(self, tx): # tx is the transaction object w method run() for cypher scripts in neo4j
+
+        cypherScriptTemplate = Template("""
+                        MATCH (:Themes)-[:HAS_THEME]->(t:Theme {name: "$theme"})
+                        RETURN t.content
                         """)
-        return [record.data() for record in result]
+
+        cypherScript = cypherScriptTemplate.safe_substitute(theme=self.theme)
+
+        # print(cypherScript)
+
+        result = tx.run(cypherScript) 
+        # print(result)
+
+        return [record.data() for record in result][0]["t.content"] # Decoding output
 
 
     def get_nodes(self):
         with self.driver.session() as session:
-            nodes = session.execute_read(self.get_anything) # Don't need to pass tx, session.execute_read injects the parameter
+            nodes = session.execute_read(self.get_body_text) # Don't need to pass tx, session.execute_read injects the parameter
             print(nodes)
-
+            # print(type(nodes))
 
         self.driver.close()  
 

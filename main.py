8b7f83c9eb2b2de2a75b57011b0d4d949592e706
Nik Vid <niklavsvisockis@fastmail.se>
2025-06-09 11:18:08 +0200

    yippi

---
+++ main.py
@@ -0,0 +1,49 @@
+key = "llx-atTZb1KavBM7EcALjbcLZtGmwWEYXEGwK9VfkjFBzwPSIW6X"
+
+from llama_cloud_services import LlamaParse
+
+class CoolParser():
+    def __init__(self, api_key=key, num_workers=4, verbose=True, language="en", result_type="markdown"):
+        self.parser = LlamaParse(api_key=api_key, num_workers=num_workers, verbose=verbose, language=language, result_type=result_type)
+        
+    def parse_page(self, file_path):
+        try:
+            result = self.parser.parse(file_path)
+            
+            markdown_documents = result.get_markdown_documents(split_by_page=True)
+
+            # get the llama-index text documents
+            text_documents = result.get_text_documents(split_by_page=False)
+
+            # get the image documents
+            image_documents = result.get_image_documents(
+                include_screenshot_images=True,
+                include_object_images=False,
+                # Optional: download the images to a directory
+                # (default is to return the image bytes in ImageDocument objects)
+                image_download_dir="./images",
+            )
+
+            # access the raw job result
+            # Items will vary based on the parser configuration
+            for page in result.pages:
+                #print(page.text)
+                print(page.md)
+                print(page.images)
+                #print(page.layout)
+                #print(page.structuredData)
+
+        except Exception as e:
+            print("Error parsing file:", e)
+
+
+# Parse a single page
+file_path = "extracted_page1.pdf"  # Replace with the path to your PDF file
+
+parser = CoolParser()
+parsed_data = parser.parse_page(file_path)
+
+
+### MAYBE:
+# Headings & subheadings could be used to navigate the document faster
+# only body text used to extract the main content when the body of text has been identified

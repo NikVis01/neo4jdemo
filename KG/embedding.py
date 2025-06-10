38917205a992ad762c5a2b733005e7625520fa8e
Nik Vid <niklavsvisockis@fastmail.se>
2025-06-10 11:48:37 +0200

    embeddings yippii

--- KG/embedding.py
+++ KG/embedding.py
@@ -2,11 +2,14 @@ import openai
 from openai import OpenAI
 import os
 
+import pandas as pd
+
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 class SickEmbedder():
     def __init__(self):
         self.text="" # Just init empty var
+        # self.csv_path = None
         self.model="text-embedding-3-small"
         self.dims=300 # Should be fine for now (masters in ML told me so)
         
@@ -18,13 +21,32 @@ class SickEmbedder():
         # Debug for embedding object
         # print(client.embeddings.create(input=[self.text], model=self.model, dimensions=self.dims).data[0])
 
+        # print(len(embeddings))
+
         return client.embeddings.create(input = [self.text], model=self.model).data[0].embedding
 
+    def embedd_df(self, input_df):
+        df = input_df
 
-    # print(len(embeddings))
+        df.fillna('Empty space', inplace=True)
 
-# EXAMPLE USAGE:
+        for i in range(df.shape[0]-1):
+            print(str(df.iloc[i,1])+"\n")
+
+            df.iloc[i, 1] = self.get_embedding_str(df.iloc[i, 1])
+
+        print(df)
+        
+        return df
+
+# EXAMPLE USAGE W STRING:
 """ 
-obj = SickEmbedder()
-obj.get_embedding_str(text="penithe")
+if __name__ == "__main__":
+    obj = SickEmbedder()
+    obj.embedd_csv(csv_path="./data/test.csv")
 """
+
+# EXAMPLE USAGE W CSV
+if __name__ == "__main__":
+    obj = SickEmbedder()
+    # obj.embedd_df()
